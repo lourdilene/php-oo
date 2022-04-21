b2a1609f14ca673f415fd03760058857f587b23d
Lourdilene Souza <lourdilene.souza@gmail.com>
2022-04-21 12:38:42 -0300

    Inclusão de endereço no titular. Composição de objetos

--- banco.php
+++ banco.php
@@ -1,10 +1,12 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Endereco.php';
 require_once 'src/Titular.php';
 require_once 'src/CPF.php';
 
-$fulano = new Titular(new CPF('123.456.789-10'), 'Fulano da Silva');
+$endereco = new Endereco('Belém', 'Um Bairro', 'Uma rua', '123');
+$fulano = new Titular(new CPF('123.456.789-10'), 'Fulano da Silva', $endereco);
 $primeiraConta = new Conta($fulano);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
@@ -13,10 +15,10 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$beltrano = new Titular(new CPF('698.549.548-10'), 'Beltrano de Souza');
+$beltrano = new Titular(new CPF('698.549.548-10'), 'Beltrano de Souza', $endereco);
 $segundaConta = new Conta($beltrano);
 var_dump($segundaConta);
 
-$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg'));
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $endereco));
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
